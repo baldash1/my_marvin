dba16a61d2f309a40ac1380bc7f0fe8f8196afea
Hugo Baldassin <hugo1.baldassin@epitech.eu>
2022-08-04 17:22:50 +0200

    [~] folder creation and github clone done, now working on seed job

--- job_dsl.groovy
+++ job_dsl.groovy
@@ -1,16 +1,50 @@
-job('Disk usage') {
+folder('Tools') {
+    displayName('Tools')
+    description('Folder for miscellaneous tools.')
+}
+
+job('Tools/clone-repository') {
+     wrappers {
+        preBuildCleanup {
+            includePattern('**/target/**')
+            deleteDirectories()
+            cleanupParameter('CLEANUP')
+        }
+    }
+    
+    parameters {
+        stringParam('GIT_REPOSITORY_URL', null, 'Git URL of the repository to clone')
+    }
+    
     steps {
-        shell('df')
+        shell('git clone $GIT_REPOSITORY_URL')
     }
 }
 
-job('Polite DDoS') {
+job('Tools/SEED') {
     parameters {
-        stringParam('NAME', 'Hogu', 'name of user')
+        stringParam('GITHUB_NAME', null, 'GitHub repository owner/repo_name (e.g.: "EpitechIT31000/chocolatine")')
+        stringParam('DISPLAY_NAME', null, 'Display name for job')
     }
+    
     steps {
-        shell('echo "Hello dear "$NAME"!"')
-        shell('date')
-        shell('echo "This is your DDoS number "$BUILD_NUMBER"."')
+        shell('git clone $GIT_REPOSITORY_URL')
     }
 }
+
+// job('Disk usage') {
+//     steps {
+//         shell('df')
+//     }
+// }
+
+// job('Polite DDoS') {
+//     parameters {
+//         stringParam('NAME', 'Hogu', 'name of user')
+//     }
+//     steps {
+//         shell('echo "Hello dear "$NAME"!"')
+//         shell('date')
+//         shell('echo "This is your DDoS number "$BUILD_NUMBER"."')
+//     }
+// }
